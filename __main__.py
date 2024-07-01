107502bbdc42307d701859ee3b26adec6316f36d
Asaduzzaman Sohel <asadlive.sohel@gmail.com>
2024-07-01 09:00:12 +0000

    first

---
+++ __main__.py
@@ -0,0 +1,23 @@
+"""A Python Pulumi program"""
+
+import pulumi
+import pulumi_aws as aws
+
+
+vpc = aws.ec2.Vpc("asad-vpc-poridi",
+    cidr_block="10.10.0.0/16",
+    enable_dns_hostnames=True,
+    enable_dns_support=True,
+    tags={
+        "Name": "AsadVpcPoridi"
+    }
+)
+
+
+
+
+
+
+
+
+
